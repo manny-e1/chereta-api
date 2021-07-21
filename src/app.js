3b72cd2ec1064f13c576d52439af84ecd9e95c0c
Amanuel Ewnetu <aewnetu6@gmail.com>
2021-07-21 22:38:18 +0300

    v1-1

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import compression from 'compression';
 
 import connectDatabase from './config/db.js';
 
@@ -19,6 +20,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(compression());
 
 app.use('/users', userRoutes);
 app.use('/items', itemRoutes);
